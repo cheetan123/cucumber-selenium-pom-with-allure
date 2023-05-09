170fefdf7f1ff086ff0034a99d5c34b935cbb1ae
Chetan Lashkari <lashkari.chetan@gmail.com>
2023-05-09 06:59:19 +0530

    commit report

---
+++ src/test/java/info/seleniumcucumber/factory/DriverFactory.java
@@ -0,0 +1,44 @@
+package info.seleniumcucumber.factory;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverFactory {
+    public WebDriver driver;
+
+    public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
+
+    /**
+     * This method is used to initialize the thradlocal driver on the basis of given
+     * browser
+     *
+     * @param browser
+     * @return this will return tldriver.
+     */
+    public WebDriver init_driver(String browser) {
+
+        System.out.println("browser value is: " + browser);
+
+        if (browser.equals("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            tlDriver.set(new ChromeDriver());
+        } else {
+            System.out.println("Please pass the correct browser value: " + browser);
+        }
+
+        getDriver().manage().deleteAllCookies();
+        getDriver().manage().window().maximize();
+        return getDriver();
+
+    }
+
+    /**
+     * this is used to get the driver with ThreadLocal
+     *
+     * @return
+     */
+    public static synchronized WebDriver getDriver() {
+        return tlDriver.get();
+    }
+}
